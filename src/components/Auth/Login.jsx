dcbf985fba3a524aeb982b729b05e673a251f409
Krishanu Mandal <krishanu178@gmail.com>
2024-10-28 23:38:14 +0530

    login design - file setup for whole project

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        console.log("Email :",email,"Password: ",password);
+        
+
+        //reset
+        setEmail("");
+        setPassword("");
+
+    }
+
+
     return (
-        <div className="flex items-center justify-center">
-            <div className="border-2 border-red-400">
-                <form action="">
-                    
+
+        <div className="flex items-center justify-center h-screen w-screen">
+            <div className="border-2 border-emerald-400 p-20 rounded-xl">
+                <form className="flex flex-col items-center justify-center" onSubmit={(e)=>submitHandler(e)}>
+                    <input value={email} onChange={(e)=>setEmail(e.target.value)} className="border-2 border-emerald-400 py-3 px-5 rounded-3xl text-xl text-white outline-none placeholder:text-gray-400" type="email" placeholder="Enter your Email" />
+                    <input value={password} onChange={(e)=>setPassword(e.target.value)} className="border-2 border-emerald-400 py-3 px-5 rounded-3xl text-xl text-white outline-none placeholder:text-gray-400 mt-5" type="password" placeholder="Enter your Password" />
+                    <button className="border-none bg-emerald-400 py-3 px-5 rounded-3xl text-xl font-bold text-white outline-none  mt-5">Login</button>
                 </form>
             </div>
             
---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,10 @@
+const EmployeeDashboard = () => {
+    
+    return (
+        <>
+            <h1 className="text-white">Employee Dahoboard</h1>
+        
+        </>
+    );
+};
+export default EmployeeDashboard;
